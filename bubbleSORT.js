3ebe2f77d78d761ec2b765c5bb2cad1b24ba4ed8
shivam pandey <satyamshivamsp@gmail.com>
2019-09-03 17:13:29 +0530

    initial update

---
+++ bubbleSORT.js
@@ -0,0 +1,23 @@
+var num = [12,11,5,18,33,9,13,44,34,23];
+
+console.log("first array :\n");
+
+for(var i=0; i<10;i++){
+    console.log(num[i]);
+}
+
+console.log("second array :\n");
+
+for(var i =0; i<10; i++){
+    var max = i;
+    for(var j =i+1; j<10; j++){
+        if(num[j] > num[max]){
+            max = j;
+        }
+    }
+    var temp = 0;
+    temp = num[i];
+    num[i] = num[max];
+    num[max] = temp;
+    console.log(num[i]);
+}
