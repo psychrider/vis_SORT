94a92ff0465ff257ee97af4070258854cb112732
shivam pandey <56724279+pandey-shivam@users.noreply.github.com>
2019-10-18 17:12:28 +0530

    Update insertionSORT.js

---
+++ insertionSORT.js
@@ -0,0 +1,23 @@
+const num = [12,11,5,18,33,9,13,44,34,23]; the array to be sorted
+
+console.log("First array :\n");
+for(var i=0; i<10; i++){
+    console.log(num[i]);
+}
+
+//the insertion sort algo
+for(var i=1; i<10; i++){
+    for(var j=i; j>0; j--){        
+        if(num[j] > num[j-1]){
+            var temp = 0;
+            temp = num[j];
+            num[j] = num[j-1];
+            num[j-1] = temp;
+        }
+    }
+}
+//final sorted array
+console.log("Sorted Array :")
+for(var i=0; i<10; i++){
+    console.log(num[i]);
+}
